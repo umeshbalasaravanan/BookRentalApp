ea04d6eeabfe514ad80fa60598695eb6db55a3ea
Umesh Balasaravanan <umesh.balasaravanan@gmail.com>
2021-03-16 17:55:31 -0400

    1. Included login activity. 2. changed app name. 3. included app icon.

--- app/src/main/java/edu/udayton/spm/MainActivity.java
+++ app/src/main/java/edu/udayton/spm/MainActivity.java
@@ -2,7 +2,10 @@ package edu.udayton.spm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView helloworld = (TextView)findViewById(R.id.login);
+
+        View.OnClickListener loginClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent login_intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login_intent);
+            }
+        };
+        helloworld.setOnClickListener(loginClickListener);
     }
 }
