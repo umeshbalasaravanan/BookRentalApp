e0046730145b1bae81f649e3361ee65b2a9e07d0
Umesh Balasaravanan <umesh.balasaravanan@gmail.com>
2021-04-26 23:33:44 -0400

    RentBooks completed successfully

--- app/src/main/java/edu/udayton/spm/RecommendedBooks.java
+++ app/src/main/java/edu/udayton/spm/RecommendedBooks.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import org.w3c.dom.Text;
 
 public class RecommendedBooks extends AppCompatActivity {
 
@@ -16,6 +21,11 @@ public class RecommendedBooks extends AppCompatActivity {
 //    public static final String detective = "false";
 //    public static final String education = "false";
 //    public static final String thriller = "false";
+
+    TextView bookName;
+    TextView authorName;
+    TextView bookPrice;
+    RoundedImageView bookImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +66,64 @@ public class RecommendedBooks extends AppCompatActivity {
 //                f2View.setVisibility(View.VISIBLE);
 //                f3View.setVisibility(View.VISIBLE);
             }
+
+            View.OnClickListener fiction1LayoutOnClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    bookName = (TextView)findViewById(R.id.fiction1BookName);
+                    authorName = (TextView)findViewById(R.id.fiction1AuthorName);
+                    bookPrice = (TextView)findViewById(R.id.fiction1price);
+                    bookImage = (RoundedImageView)findViewById(R.id.fiction1Image);
+                    Intent intent = new Intent(RecommendedBooks.this, RentBooks.class);
+                    intent.putExtra("synopsis","Ten years after her teenage daughter went missing, a mother begins a new relationship only to discover she can't truly move on until she answers lingering questions about the past. Laurel Mack's life stopped in many ways the day her 15-year-old daughter, Ellie, left the house to study at the library and never returned.");
+                    intent.putExtra("bookName",bookName.getText());
+                    intent.putExtra("author",authorName.getText());
+                    intent.putExtra("price",bookPrice.getText());
+                    intent.putExtra("image", "fiction1");
+                    startActivity(intent);
+                }
+            };
+
+            fiction1Layout.setOnClickListener(fiction1LayoutOnClickListener);
+
+            View.OnClickListener fiction2LayoutOnClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    bookName = (TextView)findViewById(R.id.fiction2BookName);
+                    authorName = (TextView)findViewById(R.id.fiction2AuthorName);
+                    bookPrice = (TextView)findViewById(R.id.fiction2price);
+                    bookImage = (RoundedImageView)findViewById(R.id.fiction2Image);
+                    Intent intent = new Intent(RecommendedBooks.this, RentBooks.class);
+                    intent.putExtra("synopsis","In The Four Winds by Kristin Hannah, Elsa Wolcott is a woman trying to raise two children on farm in the Great Plains during the Dust Bowl following the Great Depression. She watches as the lands around her crack in their perpetually parched state and the hopelessness threatens to breaks the spirit of those around her.");
+                    intent.putExtra("bookName",bookName.getText());
+                    intent.putExtra("author",authorName.getText());
+                    intent.putExtra("price",bookPrice.getText());
+                    intent.putExtra("image", "fiction2");
+                    startActivity(intent);
+                }
+            };
+
+            fiction2Layout.setOnClickListener(fiction2LayoutOnClickListener);
+
+            View.OnClickListener fiction3LayoutOnClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    bookName = (TextView)findViewById(R.id.fiction3BookName);
+                    authorName = (TextView)findViewById(R.id.fiction3AuthorName);
+                    bookPrice = (TextView)findViewById(R.id.fiction3price);
+                    bookImage = (RoundedImageView)findViewById(R.id.fiction3Image);
+                    Intent intent = new Intent(RecommendedBooks.this, RentBooks.class);
+                    intent.putExtra("synopsis","This new novel is set in the early 1980s, as the man of two faces flees not to America but to France. He’s survived a harrowing boat trip, and a flight from Jakarta with Bon, his “best friend and blood brother.” Their story is complicated to unwind.");
+                    intent.putExtra("bookName",bookName.getText());
+                    intent.putExtra("author",authorName.getText());
+                    intent.putExtra("price",bookPrice.getText());
+                    intent.putExtra("image", "fiction3");
+                    startActivity(intent);
+                }
+            };
+
+            fiction3Layout.setOnClickListener(fiction3LayoutOnClickListener);
+
             if(nf.equals("true")){
                 nonfiction1Layout.setVisibility(View.VISIBLE);
                 nonfiction2Layout.setVisibility(View.VISIBLE);
--- app/src/main/java/edu/udayton/spm/RentBooks.java
+++ app/src/main/java/edu/udayton/spm/RentBooks.java
@@ -3,6 +3,10 @@ package edu.udayton.spm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.makeramen.roundedimageview.RoundedImageView;
 
 public class RentBooks extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class RentBooks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rent_books);
+
+        Bundle myExtras = getIntent().getExtras();
+        TextView bookDesc = (TextView)findViewById(R.id.bookDesc);
+        bookDesc.setText(myExtras.getString("synopsis"));
+        TextView bookTitle = (TextView)findViewById(R.id.rentBookTitleView);
+        bookTitle.setText("Title: " + myExtras.getString("bookName"));
+        TextView bookAuthor = (TextView)findViewById(R.id.rentBookAuthorView);
+        bookAuthor.setText("author "+ myExtras.getString("author"));
+        TextView price = (TextView)findViewById(R.id.rentBookPriceView);
+        price.setText("price: " + myExtras.getString("price"));
+        RoundedImageView bookimage = (RoundedImageView)findViewById(R.id.rentBookImageView);
+        if(myExtras.getString("image").equals("fiction1")){
+            bookimage.setImageResource(R.drawable.fiction1);
+        }else if(myExtras.getString("image").equals("fiction2")){
+            bookimage.setImageResource(R.drawable.fiction2);
+        }else if(myExtras.getString("image").equals("fiction3")){
+            bookimage.setImageResource(R.drawable.fiction3);
+        }
     }
 }
